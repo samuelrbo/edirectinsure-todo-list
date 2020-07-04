b5733945dcb10ffd1d98181c52a65b8b48858329
Samuel Ramon <samuelrbo@gmail.com>
2020-07-04 20:47:54 -0300

    Remove morgan log

--- backend/src/config/server.js
+++ backend/src/config/server.js
@@ -1,19 +1,11 @@
 const path = require('path');
 const MAIN_DIR = `${path.dirname(require.main.filename)}`;
 
-const rfs = require('rotating-file-stream');
-const morgan = require('morgan');
-
 const bodyParser = require('body-parser');
 const express = require('express');
 const cors = require('cors');
 
 const jwt = require('./../app/middleware/jwt.middleware');
-
-const logStream = rfs.createStream('access.log', {
-  interval: '1d',
-  path: path.join(MAIN_DIR, 'log')
-});
 
 const app = express();
 
@@ -21,6 +13,5 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(jwt);
-app.use(morgan('combined', { stream: logStream }));
 
 module.exports = app;
